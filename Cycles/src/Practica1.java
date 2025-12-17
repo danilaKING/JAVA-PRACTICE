c1ff1691812cb445f7bcf25ef438d6b1dbb9b59f
danilaKING <gladiatorsho228@gmail.com>
2025-12-17 21:53:42 +0300

    P finished

---
+++ Cycles/src/Practica1.java
@@ -0,0 +1,12 @@
+void main() {
+    while (true) {
+        System.out.println("Этот цикл будет выполняться бесконечно");
+    }
+
+/*
+    do {
+        System.out.println("Этот цикл тоже бесконечный");
+    } while (true);
+*/
+
+}
